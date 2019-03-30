fe0d138410796bd0abbde94747e17ff3b9fb2d91
Nguyen Hoang Duong <you_create@protonmail.com>
2019-03-30 16:26:07 +0700

    Complete rewrite of script/mkimgcreds.py

--- script/mkimgcreds.py
+++ script/mkimgcreds.py
@@ -1,124 +1,86 @@
 #!/usr/bin/env python3
 # Script used to generate image credits table in CREDITS.md.
-
-from os.path import isfile
-
-# Authors
-hduong = {
-    "name": "Nguyen Hoang Duong",
-    "profile": "http://github.com/NOVAglow",
-}
-
-katst = {
-    "name": "Kat Stokes",
-    "profile": "https://unsplash.com/@katstokes_/portfolio"
-}
-
-samzell = {
-    "name": "Samuel Zeller",
-    "profile": "https://unsplash.com/@samuelzeller/portfolio"
-}
-
-# Licenses along with links
-cc0 = {
-    "name": "Creative Commons Zero v1.0 Universal",
-    "ident": "CC0",
-    "link": "https://creativecommons.org/publicdomain/zero/1.0/"
-}
-
-unsplash = {
-    "name": "The Unsplash License",
-    "link": "https://unsplash.com/license"
-}
-
-# Collection of images
+# Data is extracted from a CSV file named "img-credits.csv"
+# in the same directory as this script.
 #
-# images = [ [file_name, title, where, author, license], ... ]
-images = [
-    ["prem.jpg", "N/A", "Preamble", katst, unsplash],
-    ["folders.jpg", "N/A", "Chapter 1", samzell, unsplash],
-    ["fsh.png", "Filesystem of Linux operating systems (Visualization)",
-     "Chapter 1", hduong, cc0],
-    ["pushd-vis.png", "GNU/Linux's pushd command (Visualization)", "Chapter 1",
-     hduong, cc0],
-    ["popd-vis.png", "GNU/Linux's popd command (Visualization)", "Chapter 1",
-     hduong, cc0],
-    ["symlink-vis.png", "Symbolic link - Visualization", "Chapter 3",
-     hduong, cc0],
-    ["broken_symlink-vis.png", "Broken Symbolic link - Visualization",
-    "Chapter 3",  hduong, cc0],
-    ["hard_link-vis.png", "Hard link - Visualization", "Chapter 3",
-     hduong, cc0]
-]
+# NOTE: The output of this program may be incorrect. Please check to make sure
+# that the rendered output of the Markdown content produced by this program is
+# correct before commiting the changes.
 
-# **************************
-# * START GENERATING TABLE *
-# **************************
+from os.path import isfile, basename
+from csv import DictReader
+import logging
 
-print("<!-- This table was generated using script/mkimgcreds.py -->\n")
+csvfname = "img-credits.csv"
+
+logging.basicConfig(format="%(message)s")
+
+if not isfile(csvfname):
+    logging.critical("fatal: %s not found" % csvfname)
+    exit(1)
+
+print("<!-- This table was generated using script/%s -->\n" % basename(__file__))
 print("|Image|Metadata|")
 print("|:---:|---|")
 
-# Template for each row of table in Markdown
-template = "|![](%s) <br> %s [PNG](%s)|**Title**: %s <br> **Shown in**: %s " \
-         + "<br> **Author**: %s <br> **License**: [%s][%s]|"
+with open(csvfname, newline='', encoding='utf-8') as csvfile:
+    reader = DictReader(csvfile)
+    for row in reader:
+        # Set variables to make it easier to work with them later
+        # fname: File name of the image (including the extension)
+        # source: Possible SVG source of the image
+        # authors: (Python) List of authors
+        # org_authors: (Python) List of original authors
+        # links: (Python) List of links to authors and original authors
+        # lics: Python dictionary containing licenses' name and links to them,
+        # correspondingly
 
-# Array containing the used licenses' links.
-# This is used to create link shortcuts to the licenses.
-templic = []
+        fname = row['File name']
+        source = "svg/" + fname[:fname.rfind('.')] + ".svg"
+        authors = row['Author'].split(', ')
+        org_authors = row['Original author'].split(', ')
+        links = row['Link to author'].split(', ')
+        lics = {lic_name: lic_link
+                for lic_name, lic_link in zip(row['License'].split(", "), row['Link to license'].split(", "))}
 
-# Just like templic but for authors' profiles
-tempau = []
+        # First, work on the left cell of the row.
+        # This contains the thumbnail of the image (found in /img/thumb/), and
+        # links to different formats which the image is available under
 
-for img in images:
-    # Get SVG source within the repository of image (i.e. stored in svg/)
-    # Should be checked later if the source exists because some images might not
-    # be SVG in the first place.
-    # (img[0] -> Image title)
-    svgsrc = "svg/" + img[0][0:len(img[0]) - img[0][::-1].index(".")] + "svg"
+        print("|![]({0})<br/><br/>[{1}]({0})"
+              .format("img/thumb/" + fname, fname[fname.rfind('.') + 1:].upper()),
+              end='')
+        if isfile("../" + source):
+            print(" &mdash; [SVG](%s)" % source, end='')
+        print("|", end='')
 
-    # Get the link to the license which the image is licensed under,
-    # and add that link to templic if it hasn't been added already.
-    # (img[4] -> License (dictionary type))
-    if img[4]["link"] in templic:
-        md_imglic = "lic%d" % templic.index(img[4]["link"])
-    else:
-        templic.append(img[4]["link"])
-        md_imglic = "lic%d" % (len(templic) - 1)
+        # Work on the second cell
 
-    # Like above operation but for the author's profile link
-    if img[3]["profile"] in tempau:
-        auth_link_var = "aut%d" % tempau.index(img[3]["profile"])
-    elif img[3]["profile"] != "":
-        tempau.append(img[3]["profile"])
-        auth_link_var = "aut%d" % (len(tempau) - 1)
+        if row['Title'] != "":
+            print("**Title**: %s <br/> " % row['Title'], end='')
+        print("**Originality**: %s <br/> " % row['Originality'], end='')
+        print("**Shown in**: %s <br/> " % row['Shown in'], end='')
 
-    # Process author's field
-    # img[3] -> Image's author (dictionary type)
-    md_author = img[3]["name"]
+        print("**Author**: ", end='') if len(authors) == 1 \
+            else print("**Authors**: ", end='')
+        for author, link in zip(authors, links[:len(authors)]):
+            print("[%s](%s), " % (author, link), end='') if link != "" \
+                else print("%s, " % author, end='')
+        print("\b\b <br/> ", end='')
 
-    if img[3]["profile"][:18] == "http://github.com/":
-        # Add link to GitHub profile. The format is: "NAME (@GITHUB_USERNAME)"
-        md_author += (" ([@%s][%s])" % (img[3]["profile"][18:], auth_link_var))
-    elif img[3]["profile"] != "":
-        # Add link to any online profile if one is provided
-        md_author = "[%s][%s]" % (md_author, auth_link_var)
+        if row['Original author'] != "":
+            print("**Original author**: ", end='') if len(org_authors) == 1 \
+                else print("**Original authors**: ", end='')
+            for author, link in zip(org_authors, links[len(authors):]):
+                print("[%s](%s), " % (author, link), end='') if link != "" \
+                    else print("%s, " % author, end='')
+            print("\b\b <br/> ", end='')
 
-    # *** PRINT MARKDOWN OUTPUT FOR TABLE ROW ***
-    print(template % (
-                      "img/thumb/" + img[0],
-                      ("[SVG](%s) &mdash; " % svgsrc) if isfile("../" + svgsrc) else "",
-                      "img/" + img[0],
-                      img[1], img[2], md_author, img[4]["name"], md_imglic
-                     )
-            )
-
-# Link shortcuts to licenses
-print()
-for lic_index in range(0, len(templic)):
-    print("[lic%d]: %s" % (lic_index, templic[lic_index]))
-
-# Link shortcuts to authors' profiles
-print()
-for aut_index in range(0, len(tempau)):
-    print("[aut%d]: %s" % (aut_index, tempau[aut_index]))
+        print("**License**: ", end='') if len(lics) == 1 \
+            else print("**Licenses**: ", end='')
+        for lic_name, lic_link in lics.items():
+            print("[%s](%s), " % (lic_name, lic_link), end='')
+        print("\b\b", end='')
+        if row['Note'] != "":
+            print("<br/> **Note**: %s" % row['Note'], end='')
+        print("|")
