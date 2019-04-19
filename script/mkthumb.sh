a35541529dd70a401d9f93aeeee2402d2f159267
Nguyen Hoang Duong <you_create@protonmail.com>
2019-04-19 10:28:53 +0700

    Update script/mkthumb.sh
    Support generating images placed in subfolders of /img/

--- script/mkthumb.sh
+++ script/mkthumb.sh
@@ -2,17 +2,25 @@
 # Create thumbnails (stored in img/thumb) from PNGs found in img/
 
 imgdir="../img"  # Path of img/ relative to where this Shell script is located
-alias convert="/usr/bin/convert"  # Path to executable of ImageMagick's convert
 
 # Create img/thumb if needed
-! [ -d $imgdir/thumb ] && mkdir $imgdir/thumb
+! [ -d $imgdir/thumb ] && /usr/bin/mkdir $imgdir/thumb
 
 # Check if 'convert' command is found
 [ -z $(command -v convert) ] && \
 >&2 echo "ERROR: ImageMagick's convert utility not found" && exit 1
 
-# Iterate over PNGs found in img/ (non-recursively)
-# and use 'convert' to create thumbnails
-for imgf in $(ls -Ap $imgdir | grep -v "/$"); do
-    convert $imgdir/$imgf -resize 300 $imgdir/thumb/$imgf
+# Find all images and write their paths in .thumbs.query
+/usr/bin/find $PWD/$imgdir \
+    -not -path "$PWD/$imgdir/thumb*" \
+    -not -path "$PWD/$imgdir/.ignore*" \
+    -not -path $PWD/$imgdir \
+    > .thumbs.query
+
+# Generate thumbnails
+for imgpath in $(cat ./.thumbs.query)
+do
+    /usr/bin/convert $imgpath -resize 300 $imgdir/thumb/$(/usr/bin/basename $imgpath)
 done
+
+/usr/bin/rm -f .thumbs.query
